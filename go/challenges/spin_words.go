29c7ffec03d9e2c9cd83dc310fbf01b9ca9c4360
Francesco Pastore <francesco0599@hotmail.it>
2023-03-30 22:39:00 +0200

    feat(go): :sparkles: Add SpinWords challenge

---
+++ go/challenges/spin_words.go
@@ -0,0 +1,49 @@
+// https://www.codewars.com/kata/5264d2b162488dc400000001
+
+package kata
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/go-playground/assert/v2"
+)
+
+func reverse(s string) string {
+	new := []byte{}
+	for i := len(s); i > 0; i-- {
+		new = append(new, s[i-1])
+	}
+
+	return string(new)
+}
+
+func SpinWords(str string) string {
+	arr := strings.Split(str, " ")
+
+	for i, s := range arr {
+		if len(s) >= 5 {
+			s = reverse(s)
+		}
+
+		arr[i] = s
+	}
+
+	return strings.Join(arr, " ")
+}
+
+func TestSpinWords(t *testing.T) {
+	tests := []struct {
+		param    string
+		expected string
+	}{
+		{"Hey fellow warriors", "Hey wollef sroirraw"},
+		{"This is a test", "This is a test"},
+		{"This is another test", "This is rehtona test"},
+	}
+
+	for _, test := range tests {
+		actual := SpinWords(test.param)
+		assert.Equal(t, test.expected, actual)
+	}
+}
