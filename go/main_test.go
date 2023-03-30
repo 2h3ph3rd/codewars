d9eb2f8650ccb11d33fdeaebf66eef7546eff62f
Francesco Pastore <francesco0599@hotmail.it>
2023-03-30 22:40:14 +0200

    feat(go): :sparkles: Add template files for go

---
+++ go/main_test.go
@@ -0,0 +1,20 @@
+package kata
+
+import (
+	"testing"
+)
+
+func Test(t *testing.T) {
+	// tests := []struct {
+	// 	param    [][]int
+	// 	expected []int
+	// }{
+	// 	{[][]int{{}}, []int{}},
+	// }
+
+	// for i, test := range tests {
+	// 	actual := Snail(test.param)
+	// 	assert.Equal(t, test.expected, actual)
+	// 	fmt.Printf("test %d not passed\n", i)
+	// }
+}
